a9b4120c5543eadca5ba6d5c5d0e674f63f2cb28
Alex <alexjuliosancez@gmail.com>
2024-12-12 17:20:51 -0500

    Configuracion variables de entorno en googleSheetsService

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -3,6 +3,7 @@ import { google } from 'googleapis';
 import config from "../config/env.js";
 
 const sheets = google.sheets('v4');
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
 async function addRowToSheet(auth, spreadsheetId, values) {
     const request = {
@@ -27,12 +28,12 @@ async function addRowToSheet(auth, spreadsheetId, values) {
 const appendToSheet = async (data) => {
     try {
         const auth = new google.auth.GoogleAuth({
-            keyFile: path.join(process.cwd(), 'src/config', 'env.js'),
+            credentials,
             scopes: ['https://www.googleapis.com/auth/spreadsheets']
         });
 
         const authClient = await auth.getClient();
-        const spreadsheetId = `${config.SHEET_ID}`
+        const spreadsheetId = process.env.SHEET_ID
 
         await addRowToSheet(authClient, spreadsheetId, data);
         return 'Datos correctamente agregados'
